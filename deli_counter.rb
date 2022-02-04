332b27508b1aafa2f266ceb0e6109ae6bea9b2e0
Christy Perozzi <christy.perozzi@gmail.com>
2022-02-04 15:32:58 -0500

    commit tree 36f8a82b87efc8c363a6b5a9534c258bfba3259e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+katz_deli = []
+
+def line(arr)
+    line_list = ""
+    if arr.length > 0 
+        arr.each_with_index {|name, index| line_list << " #{index+1}. #{name}" }
+        puts "The line is currently:#{line_list}"
+    else
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(line, cust)
+    line << cust
+    puts "Welcome, #{cust}. You are number #{line.length} in line."
+end
+
+def now_serving(arr)
+    if arr.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{arr[0]}."
+        arr.shift
+    end
+end 
+
+
+#take_a_number(katz_deli, "joe")
+line(["a", "b"])
